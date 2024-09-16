7e19defce5c691dbd381a413598b1a3223369c86
musiclife <salgadosdario2@gmail.com>
2024-09-16 11:54:51 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+Window.onload = function corrercarousel() {
+    var carousel = document.getElementById('carouselExample');
+    var carouselInstance = new bootstrap.Carousel(carousel,{
+        interval: 1000
+    });
+}
+function genero() {
+    window.location.href="genero.html";
+    
+}
+function musica(){
+    window.location.href="musica.html";
+}
+ function inicio(){
+    window.location.href="index.html";
+ }
+ function guiamusica() {
+    window.location.href = 'Guiamusica.html'
+ }
+ function salsa(){
+    window.location.href = 'salsa.html'
+ }
+ function vallenato(){
+    window.location.href = 'vallenato.html'
+ }
+ function rock() {
+    window.location.href = 'rock.html'
+
+ }
+ function kpop(){
+    window.location.href = 'kpop.html'
+ }
+ function electronica(){
+    window.location.href = 'electronica.html'
+ }
+ function bachata(){
+    window.location.href = 'bachata.html'
+ }
